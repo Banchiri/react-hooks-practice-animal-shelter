7f58e108becac7ace25c2735dc038df7c63be77f
sandrabanchiri <sandrabanchiri39@gmail.com>
2024-08-28 20:39:43 +0300

    phase 2 code challenge 1

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,33 @@ import React, { useState } from "react";
 
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
+import data from "../json/db.json"
 
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+
+  //Handle filter type change
+  const onChangeType = (type) => {
+    setFilters({ ...filters, type });
+  };
+  // Fetch and filter pets based on the selected type
+  const onFindPetsClick = () => {
+    let filteredPets = data.pets;
+    if (filters.type !== "all") {
+      filteredPets = data.pets.filter((pet) => pet.type === filters.type);
+    }
+    setPets(filteredPets);
+  };
+
+  // Handle pet adoption
+  const onAdoptPet = (id) => {
+    const updatedPets = pets.map((pet) =>
+      pet.id === id ? { ...pet, isAdopted: true } : pet
+    );
+    setPets(updatedPets);
+  };
+
 
   return (
     <div className="ui container">
@@ -15,10 +38,13 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters 
+              onChangeType={onChangeType} 
+              onFindPetsClick={onFindPetsClick} 
+            />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={onAdoptPet} />
           </div>
         </div>
       </div>
